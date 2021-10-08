b7f605f98458b8df94ff1030e2b964d868fdc261
eugeniagarcia <eugegarciaruiz2@gmail.com>
2021-10-08 02:19:44 -0300

    abUsuarioRol.php

---
+++ control/Session.php
@@ -0,0 +1,18 @@
+<?
+
+/*3. Implementar dentro de la capa de Control la clase Session con los siguientes métodos:
+• _ _construct(). Constructor que. Inicia la sesión.
+• iniciar($nombreUsuario,$psw). Actualiza las variables de sesión con los valores ingresados.
+• validar(). Valida si la sesión actual tiene usuario y psw válidos. Devuelve true o false.
+• activa(). Devuelve true o false si la sesión está activa o no. 
+• getUsuario().Devuelve el usuario logeado.
+• getRol(). Devuelve el rol del usuario logeado.
+• cerrar(). Cierra la sesión actual.
+ */
+
+
+
+class Session {
+  	
+    
+}
